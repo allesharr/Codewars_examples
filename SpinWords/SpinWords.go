f953f9597c3059cbe793755fc758bb0cef61295a
allesharr <allesharr@gmail.com>
2022-06-28 13:57:58 +0300

    Added func SpinWords

---
+++ SpinWords/SpinWords.go
@@ -0,0 +1,24 @@
+package spinwords
+
+import (
+	"strings"
+)
+
+//Si longitude de la linea es 5 o mas de 5 symbolos -> tienes que volver la linea
+func SpinWords(str string) string {
+	ss := strings.Split(str, " ")
+	if strings.Contains(str)
+	var return_string string = ""
+	for _, element := range ss {
+		if len(element) > 4 {
+			for i := 0; i < len(element); i++ {
+				b := element[len(element)-i-1]
+				return_string = return_string + string(b)
+			}
+			return_string = return_string + " "
+		} else {
+			return_string = return_string + element + " "
+		}
+	}
+	return return_string
+}
